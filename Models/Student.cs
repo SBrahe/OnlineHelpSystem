e94742b1d0c6b0531eb050327c1176fd98712d19
SBrahe <201807173@post.au.dk>
2020-04-09 10:48:39 +0200

    Added models

---
+++ Models/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineHelpSystem.Models
+{
+    class Student
+    {
+        public string AuId { get; set; }
+        public string Name { get; set; }
+
+    }
+}
