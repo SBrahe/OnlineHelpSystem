ed1945d271ade10eb7a11b9524590bd48e89c76d
FDalager <54621401+FDalager@users.noreply.github.com>
2020-04-09 12:00:22 +0200

    Startet på DBContext

--- Data/MyDbContext.cs
+++ Data/MyDbContext.cs
@@ -11,5 +11,17 @@ namespace OnlineHelpSystem.Data
         {
             optionsBuilder.UseSqlServer("Data Source = 127.0.0.1, 1433; Database = OnlineHelpSystemDb; User ID = SA; Password = SecurePassword1!");
         }
+
+        public DbSet<Assignment> Assignments { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Exercise> Exersices { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public  DbSet<Teacher> Teachers { get; set; }
+
+        protected override void OnModelCCreating(ModelBuilder mb)
+        {
+
+        }
+
     }
 }
