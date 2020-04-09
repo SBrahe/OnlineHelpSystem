831e7df419d4782fa64de0adbb6364d4707b8aa3
SBrahe <201807173@post.au.dk>
2020-04-09 11:11:56 +0200

    Added DbContext

---
+++ Data/MyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineHelpSystem.Data
+{
+    class MyDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source = 127.0.0.1, 1433; Database = ProductDB; User ID = SA; Password = SecurePassword1!");
+        }
+    }
+}
