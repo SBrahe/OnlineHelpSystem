7b11e9fc62e7fa46c0f89e2f3faeb9b762315bd1
oskar <oskar.vedel@gmail.com>
2020-04-09 12:11:33 +0200

    updated gitignore

--- Data/MyDbContext.cs
+++ Data/MyDbContext.cs
@@ -9,19 +9,7 @@ namespace OnlineHelpSystem.Data
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Data Source = 127.0.0.1, 1433; Database = OnlineHelpSystemDb; User ID = SA; Password = SecurePassword1!");
+            optionsBuilder.UseSqlServer("Data Source=127.0.0.1,1433;Database=Oskars_Hemmelige_Database;User ID=SA;Password=1Hemmeligkode;");
         }
-
-        public DbSet<Assignment> Assignments { get; set; }
-        public DbSet<Course> Courses { get; set; }
-        public DbSet<Exercise> Exersices { get; set; }
-        public DbSet<Student> Students { get; set; }
-        public  DbSet<Teacher> Teachers { get; set; }
-
-        protected override void OnModelCCreating(ModelBuilder mb)
-        {
-
-        }
-
     }
 }
