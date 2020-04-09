1c9d915e010b2f4c7adc10eb3bc5e13e6dc68b8d
SBrahe <201807173@post.au.dk>
2020-04-09 14:29:32 +0200

    studentassignment

---
+++ Models/StudentAssignment.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineHelpSystem.Models
+{
+    class StudentAssignment
+    {
+        public string AuId { get; set; }
+        public Student Student { get; set; }
+    }
+}
