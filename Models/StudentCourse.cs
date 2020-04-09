94489881414abd5aa4225fc1c87849b6893d6fc3
SBrahe <201807173@post.au.dk>
2020-04-09 14:02:16 +0200

    added StudentCourse

---
+++ Models/StudentCourse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OnlineHelpSystem.Models
+{
+    class StudentCourse
+    {
+        public int Semester { get; set; }
+        public bool IsActive { get; set; }
+
+        public string AuId { get; set; }
+        public Student Student { get; set; }
+        public string CourseId { get; set; }
+        public Course Course { get; set; }
+    }
+}
