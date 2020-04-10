9b017640e791fef07f9b384d4917885e8685cdc8
SBrahe <201807173@post.au.dk>
2020-04-10 13:18:15 +0200

    added HelpRequest, maybe wrong?

--- Models/HelpRequest.cs
+++ Models/HelpRequest.cs
@@ -6,5 +6,14 @@ namespace OnlineHelpSystem.Models
 {
     class HelpRequest
     {
+        public string AuId { get; set; }
+        public Student Student { get; set; }
+
+        public int AssignmentNumber { get; set; }
+        public Assignment assignment { get; set; }
+
+        public int Number { get; set; }
+        public string Lecture { get; set; }
+        public Exercise Exercise { get; set; }
     }
 }
